8901b592cd482cab22aed68f4d753bcc245d6187
mohammedjabir <mohammedjabir@192.168.0.108>
2025-01-02 17:45:20 +0530

    SauceLab Tsest Suite

---
+++ Test Suites/SauceLab TS 01.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SauceLab TS 01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a083e142-1dc5-44ae-9f33-bd6dd5aaa5c9</testSuiteGuid>
+</TestSuiteEntity>
