5fe5681dfa5081cd8652883b67c0ccb36d13abb6
mohammedjabir <mohammedjabir@192.168.0.103>
2025-04-02 19:12:54 +0530

    GitHub Actions

---
+++ Test Suites/GitHub Actions.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>GitHub Actions</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>080f56dd-7b0d-44f1-aecf-c2fdbc254c66</testSuiteGuid>
+   <testCaseLink>
+      <guid>f1bf9d80-7d6c-4617-8745-06e33c8424ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
